40110f5ab1732039dc1746c0325422ba3e1b253a
赵春宇 <zhaochunyu@zhaochunyudeMacBook-Pro.local>
2017-02-15 15:19:38 +0800

    导航

--- MyApplication/app/src/main/java/com/example/dllo/gift/huyang/myapplication/Main3Activity.java
+++ MyApplication/app/src/main/java/com/example/dllo/gift/huyang/myapplication/Main3Activity.java
@@ -3,13 +3,11 @@ package com.example.dllo.gift.huyang.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
     }
-    //打上了高冷的
-    
 }
