c8166751567e1a6de5b39753eb6699a3ee603064
dllo <878755928@qq.com>
2017-02-15 14:45:03 +0800

    支付功能完成

---
+++ MyApplication/app/src/main/java/com/example/dllo/gift/huyang/myapplication/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.dllo.gift.huyang.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    //打上了高冷的
+}
